48b3bbeb9b66abf390e84c936abda1b86bb13cfd
ElCapone07 <35300244+ElCapone07@users.noreply.github.com>
2018-01-15 15:23:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on('message', message => {
             if (randnum == 0){
                 const hug0_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('http://i.imgur.com/qSejAy0.gif')
                 message.channel.sendEmbed(hug0_embed)
                 console.log('commande hug0 executé')
@@ -36,7 +36,7 @@ bot.on('message', message => {
             if (randnum == 1){
                 const hug1_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/CyW6NTEfSw288/source.gif')
                 message.channel.sendEmbed(hug1_embed)
                 console.log('commande hug1 executé')
@@ -45,7 +45,7 @@ bot.on('message', message => {
             if (randnum == 2){
                 const hug2_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/tZiqGOYhRw27m/giphy.gif')
                 message.channel.sendEmbed(hug2_embed)
                 console.log('commande hug2 executé')
@@ -54,7 +54,7 @@ bot.on('message', message => {
             if (randnum == 3){
                 const hug3_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/LWTxLvp8G6gzm/giphy.gif')
                 message.channel.sendEmbed(hug3_embed)
                 console.log('commande hug3 executé')
@@ -63,7 +63,7 @@ bot.on('message', message => {
             if (randnum == 4){
                 const hug4_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('http://gifimage.net/wp-content/uploads/2017/01/Anime-hug-GIF-Image-Download-24.gif')
                 message.channel.sendEmbed(hug4_embed)
                 console.log('commande hug4 executé')
@@ -72,7 +72,7 @@ bot.on('message', message => {
             if (randnum == 5){
                 const hug5_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/EwEeA6fagZNHW/giphy.gif')
                 message.channel.sendEmbed(hug5_embed)
                 console.log('commande hug5 executé')
@@ -81,7 +81,7 @@ bot.on('message', message => {
             if (randnum == 6){
                 const hug6_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://i.pinimg.com/originals/68/0b/69/680b69563aceba3df48b4483d007bce3.gif')
                 message.channel.sendEmbed(hug6_embed)
                 console.log('commande hug6 executé')
@@ -90,7 +90,7 @@ bot.on('message', message => {
             if (randnum == 7){
                 const hug7_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/EbWgNTFqb9Muk/source.gif')
                 message.channel.sendEmbed(hug7_embed)
                 console.log('commande hug7 executé')
@@ -99,7 +99,7 @@ bot.on('message', message => {
             if (randnum == 8){
                 const hug8_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://myanimelist.cdn-dena.com/s/common/uploaded_files/1461068486-646f3523d0fd8f3e6d818d96012b248e.gif')
                 message.channel.sendEmbed(hug8_embed)
                 console.log('commande hug8 executé')
@@ -108,7 +108,7 @@ bot.on('message', message => {
             if (randnum == 9){
                 const hug9_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://78.media.tumblr.com/5fa89acc3637b84a29c6f528c1727135/tumblr_o3kyyxj5nF1unech7o1_500.gif')
                 message.channel.sendEmbed(hug9_embed)
                 console.log('commande hug9 executé')
@@ -117,7 +117,7 @@ bot.on('message', message => {
             if (randnum == 10){
                 const hug10_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/yziFo5qYAOgY8/giphy.gif')
                 message.channel.sendEmbed(hug10_embed)
                 console.log('commande hug10 executé')
@@ -126,7 +126,7 @@ bot.on('message', message => {
             if (randnum == 11){
                 const hug11_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://i.imgur.com/gw3hsqe.gif')
                 message.channel.sendEmbed(hug11_embed)
                 console.log('commande hug11 executé')
@@ -135,7 +135,7 @@ bot.on('message', message => {
 			if (randnum == 12){
                 const hug12_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('http://i.imgur.com/qSejAy0.gif')
                 message.channel.sendEmbed(hug12_embed)
                 console.log('commande hug12 executé')
@@ -144,7 +144,7 @@ bot.on('message', message => {
             if (randnum == 13){
                 const hug13_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/CyW6NTEfSw288/source.gif')
                 message.channel.sendEmbed(hug13_embed)
                 console.log('commande hug13 executé')
@@ -153,7 +153,7 @@ bot.on('message', message => {
             if (randnum == 14){
                 const hug14_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/tZiqGOYhRw27m/giphy.gif')
                 message.channel.sendEmbed(hug14_embed)
                 console.log('commande hug14 executé')
@@ -162,7 +162,7 @@ bot.on('message', message => {
             if (randnum == 15){
                 const hug15_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/LWTxLvp8G6gzm/giphy.gif')
                 message.channel.sendEmbed(hug15_embed)
                 console.log('commande hug15 executé')
@@ -171,7 +171,7 @@ bot.on('message', message => {
             if (randnum == 16){
                 const hug16_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media1.tenor.com/images/c32141ae982029beaf8db8d4ddf057bd/tenor.gif?itemid=5960635')
                 message.channel.sendEmbed(hug16_embed)
                 console.log('commande hug16 executé')
@@ -180,7 +180,7 @@ bot.on('message', message => {
             if (randnum == 17){
                 const hug17_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/EwEeA6fagZNHW/giphy.gif')
                 message.channel.sendEmbed(hug17_embed)
                 console.log('commande hug17 executé')
@@ -189,7 +189,7 @@ bot.on('message', message => {
             if (randnum == 18){
                 const hug18_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://i.pinimg.com/originals/68/0b/69/680b69563aceba3df48b4483d007bce3.gif')
                 message.channel.sendEmbed(hug18_embed)
                 console.log('commande hug18 executé')
@@ -198,7 +198,7 @@ bot.on('message', message => {
             if (randnum == 19){
                 const hug19_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://media.giphy.com/media/EbWgNTFqb9Muk/source.gif')
                 message.channel.sendEmbed(hug19_embed)
                 console.log('commande hug19 executé')
@@ -207,7 +207,7 @@ bot.on('message', message => {
             if (randnum == 20){
                 const hug20_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://myanimelist.cdn-dena.com/s/common/uploaded_files/1461068486-646f3523d0fd8f3e6d818d96012b248e.gif')
                 message.channel.sendEmbed(hug20_embed)
                 console.log('commande hug20 executé')
@@ -216,7 +216,7 @@ bot.on('message', message => {
             if (randnum == 21){
                 const hug21_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://78.media.tumblr.com/5fa89acc3637b84a29c6f528c1727135/tumblr_o3kyyxj5nF1unech7o1_500.gif')
                 message.channel.sendEmbed(hug21_embed)
                 console.log('commande hug21 executé')
@@ -225,7 +225,7 @@ bot.on('message', message => {
             if (randnum == 22){
                 const hug22_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('http://pa1.narvii.com/5662/7614d83e68f9630de0f5a7a51690a73a42b529d6_hq.gif')
                 message.channel.sendEmbed(hug22_embed)
                 console.log('commande hug22 executé')
@@ -234,7 +234,7 @@ bot.on('message', message => {
             if (randnum == 23){
                 const hug23_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://i.imgur.com/gw3hsqe.gif')
                 message.channel.sendEmbed(hug23_embed)
                 console.log('commande hug23 executé')
@@ -243,7 +243,7 @@ bot.on('message', message => {
             if (randnum == 24){
                 const hug24_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://i.skyrock.net/9531/83199531/pics/3087048637_1_3_4dv7ysMK.gif')
                 message.channel.sendEmbed(hug24_embed)
                 console.log('commande hug24 executé')
@@ -252,7 +252,7 @@ bot.on('message', message => {
             if (randnum == 25){
                 const hug25_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://myanimelist.cdn-dena.com/s/common/uploaded_files/1460993069-9ac8eaae8cd4149af4510d0fed0796bf.gif')
                 message.channel.sendEmbed(hug25_embed)
                 console.log('commande hug25 executé')
@@ -261,7 +261,7 @@ bot.on('message', message => {
             if (randnum == 26){
                 const hug26_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('http://media1.giphy.com/media/QbkL9WuorOlgI/giphy.gif')
                 message.channel.sendEmbed(hug26_embed)
                 console.log('commande hug26 executé')
@@ -270,7 +270,7 @@ bot.on('message', message => {
             if (randnum == 27){
                 const hug27_embed = new Discord.RichEmbed()
                 .setColor(15105570)
-                .addField(message.author + " offre un câlin à " + mentionned.username ,"On aime se donner des câlins")
+                .addField("**" + message.author.username + "**" + " offre un câlin à **" + mentionned.username + "**","On aime se donner des câlins :hugging: ")
                 .setImage('https://lh3.googleusercontent.com/-w_NhaCQ6Rcc/VlECUNpi8CI/AAAAAAABPCY/c3rXwR-4ji0/w500-h281-n/tumblr_nw71abB0zu1ru30ujo1_500.gif')
                 message.channel.sendEmbed(hug27_embed)
                 console.log('commande hug27 executé')
